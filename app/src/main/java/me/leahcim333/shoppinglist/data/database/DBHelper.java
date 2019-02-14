693f5c87c953ed003e6e3f0cfe1fcc86b79a6205
Michał Domino <leahcim333@o2.pl>
2019-02-14 12:20:10 +0100

    Added inserting data to database.

--- app/src/main/java/me/leahcim333/shoppinglist/data/database/DBHelper.java
+++ app/src/main/java/me/leahcim333/shoppinglist/data/database/DBHelper.java
@@ -1,5 +1,6 @@
 package me.leahcim333.shoppinglist.data.database;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -9,7 +10,7 @@ public class DBHelper extends SQLiteOpenHelper {
     public static final int DATABASE_VERSION = 1;
     public static final String DATABASE_NAME = "dataBase.db";
     public static final String TABLE_NAME = "productTB";
-    
+
     public static final String COLUMN_NAME_ID = "ID";
     public static final String COLUMN_NAME_CHECKED = "CHECKED";
     public static final String COLUMN_NAME_PRODUCT_NAME = "PRODUCT_NAME";
@@ -26,6 +27,7 @@ public class DBHelper extends SQLiteOpenHelper {
 
     public DBHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        SQLiteDatabase db = this.getWritableDatabase();
 
     }
 
@@ -37,5 +39,14 @@ public class DBHelper extends SQLiteOpenHelper {
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL(SQL_DELETE_ENTRIES);
+    }
+
+    public boolean insertData(Long checked, String productName) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COLUMN_NAME_CHECKED, checked);
+        contentValues.put(COLUMN_NAME_PRODUCT_NAME, productName);
+        long result = db.insert(TABLE_NAME, null, contentValues);
+        return result != -1;
     }
 }
